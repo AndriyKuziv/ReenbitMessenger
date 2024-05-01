f7ea2f95e3690b0043d04d903bed2b6b74247bb5
Andrii Kuziv <happyandreas18@gmail.com>
2024-05-01 10:54:48 +0300

    Added integration tests WIP; minor refactoring

---
+++ ReenbitMessenger.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReenbitMessenger.API.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> TestConnection()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
